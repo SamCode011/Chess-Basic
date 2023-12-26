4f6220abe60d040b691e153251bacda15486f1de
SamCode011 <aaron2000-2000@outlook.com>
2023-12-26 18:31:44 -0500

    insert colors-pice  from chess.js JS

--- chess.js
+++ chess.js
@@ -20,6 +20,7 @@ function createBoard() {
         const square = document.createElement('div')
         square.classList.add('square')
         square.innerHTML = startPiece
+        square.firstChild?.setAttribute('draggable',true)
         square.setAttribute('square-id', i)
        /*  square.classList.add('beige') */
        const row =Math.floor((63 - i )/ 8) + 1
@@ -28,7 +29,25 @@ function createBoard() {
        }else{
         square.classList.add(i % 2 === 0 ? "brown" : "beige")
        }
+
+       if(i <= 15){
+        square.firstChild.firstChild.classList.add('black')
+       }
+       if(i >= 48){
+        square.firstChild.firstChild.classList.add('white')
+       }
         gameBoard.append(square)
     })
 }
 createBoard()
+
+const allSquares = document.querySelectorAll("#gameboard .square")
+
+
+allSquares.forEach(square => {
+    square.addEventListener('dragstart',dragStart)
+})
+
+function dragStart () {
+    
+}
