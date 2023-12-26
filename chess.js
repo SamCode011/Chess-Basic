1e830103f9d9f91f5f6ad89bc85bec754bce09c4
SamCode011 <aaron2000-2000@outlook.com>
2023-12-26 17:55:11 -0500

    styles.css and chess.js

--- chess.js
+++ chess.js
@@ -3,20 +3,26 @@ const playerDisplay = document.querySelector('#player')
 const infoDisplay = document.querySelector('#info-display')
 const width = 8
 
-const startPieces = [
-    rook,knight,bishop,queen,king,bishop,knight,rook,
+const startPieces=[
+    rook, knight, bishop, queen, king, bishop, knight, rook,
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
-    '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '',
-    '', '', '', '', '', '', '', '',
+    '',  '',  '',  '',  '',  '',  '', '',
+    '',  '',  '',  '',  '',  '',  '', '',
+    '',  '',  '',  '',  '',  '',  '', '',
+    '',  '',  '',  '',  '',  '',  '', '',
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
-    rook,knight,bishop,queen,king,bishop,knight,rook
+    rook, knight, bishop, queen, king, bishop, knight, rook
+
 ]
 
 function createBoard() {
-    startPieces.forEach((startPieces) => {
+    startPieces.forEach((startPiece, i) => {
         const square = document.createElement('div')
         square.classList.add('square')
+        square.innerHTML = startPiece
+        square.setAttribute('square-id', i)
+        square.classList.add('beige')
+        gameBoard.append(square)
     })
 }
+createBoard()
