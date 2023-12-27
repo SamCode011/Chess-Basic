36839418d1d993c5da24b0e0b09008e3dbe664dd
SamCode011 <aaron2000-2000@outlook.com>
2023-12-27 01:30:04 -0500

    javascript Js

--- chess.js
+++ chess.js
@@ -1,3 +1,4 @@
+// @language=javascript
 const gameBoard = document.querySelector('#gameboard')
 const playerDisplay = document.querySelector('#player')
 const infoDisplay = document.querySelector('#info-display')
