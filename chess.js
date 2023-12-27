7469adcaad94990b384e6d9547297347e80fd3c4
SamCode011 <aaron2000-2000@outlook.com>
2023-12-26 19:40:01 -0500

    insert logics check

--- chess.js
+++ chess.js
@@ -2,6 +2,8 @@ const gameBoard = document.querySelector('#gameboard')
 const playerDisplay = document.querySelector('#player')
 const infoDisplay = document.querySelector('#info-display')
 const width = 8
+let playerGo = 'black'
+playerDisplay.textContent = 'black'
 
 const startPieces=[
     rook, knight, bishop, queen, king, bishop, knight, rook,
@@ -41,7 +43,7 @@ function createBoard() {
 }
 createBoard()
 
-const allSquares = document.querySelectorAll("#gameboard .square")
+const allSquares = document.querySelectorAll(".square")
 
 
 allSquares.forEach(square => {
@@ -64,8 +66,56 @@ function dragOver(e) {
 
 function dragDrop(e) {
     e.stopPropagation()
+    /* console.log(draggedElement) */
+    const correctGo = draggedElement.firstChild.classList.contains(playerGo)
+    const taken = e.target.classList.contains('piece')
+    const opponentGo =playerGo === 'white' ? 'black' : 'white'
+    const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo)
 
-    //e.target.parentNode.append(draggedElement)
-    e.target.append(draggedElement)
+
+    if(correctGo) {
+        // must check this first 
+        if(takenByOpponent && valid) {
+             e.target.parentNode.append(draggedElement)
+             e.target.remove()
+             changePlayer()
+             return
+        }
+
+        //then check this
+        if(taken && !takenByOpponent){
+            infoDisplay.textContent = "you cannot go here!"
+            setTimeout(() => infoDisplay.textContent = "",2000)
+            return
+        }
+        if (valid) {
+            e.target.append(draggedElement)
+            changePlayer()
+            return
+        }
+    }  
+}
+
+function changePlayer() {
+    if(playerGo === 'black') {
+        reverseIds() 
+        playerGo = "white"
+        playerDisplay.textContent = 'white'
+    } else {
+        revertIds()
+        playerGo = 'black'
+        playerDisplay.textContent = 'black'
+    }
+}
+
+function reverseIds() {
+    const allSquares = document.querySelectorAll('.square')
+    allSquares.forEach((square, i) =>
+    square.setAttribute('square-id',(width * width - 1) -i))
+}
+
+function revertIds() {
+    const allSquares = document.querySelectorAll('.square')
+    allSquares.forEach((square, i) => square.setAttribute('square-id',i))
 
 }
